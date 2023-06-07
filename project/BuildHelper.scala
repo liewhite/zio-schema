f5016b1202eeb3730cc3932d3b1000826f82f610
李霖 <lee@lilindeMacBook-Pro.local>
2023-06-07 17:55:53 +0800

    zio version

--- project/BuildHelper.scala
+++ project/BuildHelper.scala
@@ -10,7 +10,7 @@ object BuildHelper {
 
   val Scala3: String   = "3.2.2" //versions.getOrElse("3.0", versions("3.1"))
 
-  val zioVersion                   = "2.0.15"
+  val zioVersion                   = "2.0.13"
   val zioJsonVersion               = "0.5.0"
   val zioPreludeVersion            = "1.0.0-RC18"
   val zioOpticsVersion             = "0.2.1"
